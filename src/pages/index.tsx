81785e4a4584578ed6360bb3e7c5a540bcb1092b
yond1994 <yond1994@gmail.com>
2023-07-10 16:10:25 +0200

    add bootstrap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [sessionToken, setSessionToken] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loginForm, setLoginForm] = useState({
-    username: "",
+    email: "",
     password: "",
   });
 
@@ -23,20 +23,21 @@ export default function Home() {
   };
 
   const handleSubmit = async () => {
-    if (loginForm.username.length <= 0)
-      return alert("el username no puede estar vacio");
+    if (loginForm.email.length <= 0)
+      return alert("el email no puede estar vacio");
     if (loginForm.password.length <= 0)
       return alert("el password no puede estar vacio");
 
     try {
-      const { data } = await axios.post("/api/login", loginForm);
+      const { data } = await axios.post(process.env.NEXT_PUBLIC_API + "/auth/login", loginForm);
       // data nos retornara si el login ha sido correcto y un token de sesion de ser asi
       console.log(data);
       setLoginForm({
         password: "",
-        username: "",
+        email: "",
       });
       setIsLoggedIn(true);
+      console.log(data.token);
       setSessionToken(data.token);
     } catch (error) {
       alert(error);
@@ -44,8 +45,8 @@ export default function Home() {
   };
 
   const [createProjectFrom, setCreateProjectFrom] = useState({
-    projectName: "",
-    imageUrl: "",
+    name: "",
+    img: "",
   });
 
   const handleProjectFormChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -57,14 +58,15 @@ export default function Home() {
 
   const createProject = async () => {
     try {
-      const { data } = await axios.post("/api/project", createProjectFrom, {
+      const { data } = await axios.post(process.env.NEXT_PUBLIC_API + "/projects", createProjectFrom, {
         headers: {
           token: sessionToken,
+          Authorization: 'Bearer ' + sessionToken
         },
       });
       setCreateProjectFrom({
-        imageUrl: "",
-        projectName: "",
+        img: "",
+        name: "",
       });
       console.log(data);
       alert("projecto generado");
@@ -82,15 +84,15 @@ export default function Home() {
           <input
             type="text"
             onChange={(e) => handleProjectFormChange(e)}
-            name="projectName"
-            value={createProjectFrom.projectName}
+            name="name"
+            value={createProjectFrom.name}
           />
           <label>url de la imagen</label>
           <input
             type="text"
             onChange={(e) => handleProjectFormChange(e)}
-            name="imageUrl"
-            value={createProjectFrom.imageUrl}
+            name="img"
+            value={createProjectFrom.img}
           />
           <button onClick={createProject}>create project</button>
         </div>
@@ -110,15 +112,39 @@ export default function Home() {
 
   return (
     <>
+    
       <div className={styles.loginContainer}>
+        <div className="d-flex align-items-center py-4 bg-body-tertiary">
+        <main className="form-signin w-100 m-auto">
+          <form>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div className="form-floating">
+              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+              <label>Email address</label>
+            </div>
+            <div className="form-floating">
+              <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+              <label >Password</label>
+            </div>
+
+
+            <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+            <p className="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+          </form>
+        </main>
+        </div>
+     
+
+
         <h2>Login</h2>
         <div className={styles.loginFormContainer}>
-          <label>username</label>
+          <label>Email</label>
           <input
             type="text"
-            name="username"
+            name="email"
             onChange={(e) => handleFormChange(e)}
-            value={loginForm.username}
+            value={loginForm.email}
           />
           <label> password</label>
           <input
