c55ab98d9e9db32a4ff525106300ac4f6f3d0091
yond1994 <yond1994@gmail.com>
2023-07-20 20:30:46 +0200

    new modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,29 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Script from "next/script";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return(
     <>
     <Head>
       <title>Panel portfolio</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-          crossOrigin="anonymous"
-        />
+      {/* iconos */}
+      <link 
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+      crossOrigin="anonymous"
+      referrerPolicy="no-referrer" />
+
     </Head>
-    <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossOrigin="anonymous"
-      />
     <Component {...pageProps} />
     </>
   ) 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,14 +22,14 @@ export default function Home() {
     });
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     let session = localStorage.getItem("TokenSession") || "";
-   
-    if(session) {
+
+    if (session) {
       setIsLoggedIn(true);
-      setSessionToken(session); 
+      setSessionToken(session);
     }
-  }, [])
+  }, []);
 
   const handleSubmit = async () => {
     if (loginForm.email.length <= 0)
@@ -38,9 +38,12 @@ export default function Home() {
       return alert("el password no puede estar vacio");
 
     try {
-      const { data } = await axios.post( process.env.NEXT_PUBLIC_API + "/auth/login", loginForm);
+      const { data } = await axios.post(
+        process.env.NEXT_PUBLIC_API + "/auth/login",
+        loginForm
+      );
       // data nos retornara si el login ha sido correcto y un token de sesion de ser asi
-      console.log("🚀 ~ file: index.tsx:35 ~ handleSubmit ~ data:", data)
+      console.log("🚀 ~ file: index.tsx:35 ~ handleSubmit ~ data:", data);
       setLoginForm({
         password: "",
         email: "",
@@ -56,7 +59,9 @@ export default function Home() {
   };
   async function getProjects() {
     try {
-      const { data } = await axios.get(process.env.NEXT_PUBLIC_API + "/projects");
+      const { data } = await axios.get(
+        process.env.NEXT_PUBLIC_API + "/projects"
+      );
       console.log("🚀 ~ file: index.tsx:53 ~ getProjects ~ data:", data);
       setState(data);
     } catch (error) {
@@ -70,9 +75,10 @@ export default function Home() {
   const [createProjectFrom, setCreateProjectFrom] = useState({
     name: "",
     img: "",
+    description: ""
   });
 
-  const handleProjectFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleProjectFormChange = (e: ChangeEvent<HTMLInputElement|HTMLTextAreaElement >) => {
     setCreateProjectFrom({
       ...createProjectFrom,
       [e.target.name]: e.target.value,
@@ -81,131 +87,184 @@ export default function Home() {
 
   const createProject = async () => {
     try {
-      const { data } = await axios.post(process.env.NEXT_PUBLIC_API + "/projects", createProjectFrom, {
-        headers: {
-          token: sessionToken,
-          Authorization: 'Bearer '+ sessionToken
-        },
-      });
+      const { data } = await axios.post(
+        process.env.NEXT_PUBLIC_API + "/projects",
+        createProjectFrom,
+        {
+          headers: {
+            token: sessionToken,
+            Authorization: "Bearer " + sessionToken,
+          },
+        }
+      );
       setCreateProjectFrom({
         img: "",
         name: "",
+        description: "",
       });
       getProjects();
-     
     } catch (error) {
       console.log(error);
     }
   };
+
+  const showModal = () => {
+    const { Modal } = require("bootstrap");
+    const myModal = new Modal("#exampleModal");
+
+    myModal.show();
+  };
+
+  const editModal = (_id: string, name:string, img: string, description:string ) => {
+    // edit modal
+    console.log(_id, name, img, description);
+    setCreateProjectFrom({
+      img,
+      name,
+      description,
+    });
+
+    showModal();
+  }
 
   if (isLoggedIn)
     return (
       <>
         <div className="container">
           <div className="row">
-            
-              <div className="d-flex align-items-center p-3 my-3 text-white bg-black rounded shadow-sm ">
-                <img
-                  className="me-3"
-                  src="https://moon.ly/uploads/nft/clg20i5ys7518u5jsdr1xcqgx.png"
-                  alt=""
-                  width="48"
-                  height="38"
-                />
-                <div className="lh-1">
-                  <h1 className="h6 mb-0 text-white lh-1">Bootstrap</h1>
-                  <small>Since 2011</small>
-                </div>
+            <div className="d-flex align-items-center p-3 my-3 text-white bg-black rounded shadow-sm ">
+              <img
+                className="me-3"
+                src="https://moon.ly/uploads/nft/clg20i5ys7518u5jsdr1xcqgx.png"
+                alt=""
+                width="48"
+                height="38"
+              />
+              <div className="lh-1">
+                <h1 className="h6 mb-0 text-white lh-1">Bootstrap</h1>
+                <small>Since 2011</small>
               </div>
+            </div>
 
-              <div className="my-3 p-3 bg-body rounded shadow-sm">
-                <h6 className="border-bottom pb-2 mb-0">Proyectos</h6>
+            <div className="my-3 p-3 bg-body rounded shadow-sm">
+              <div className="row border-bottom">
+                <div className="col-9">
+                <h3 className=" pb-2 mb-0">Proyectos</h3>
+                </div>
+                <div className="col-3">
+                  <button type="button" className="btn btn-primary mb-2" onClick={showModal}>
+                    Crear proyecto
+                  </button>
+                </div> 
+              </div>
+             
 
-                {state
-                  ? state.map((p: any) => (
-                      <div
-                        key={p.id}
-                        className="d-flex text-body-secondary pt-3"
-                      >
-                        {/* <svg
-                          className="bd-placeholder-img flex-shrink-0 me-2 rounded"
-                          width="32"
-                          height="32"
-                          xmlns="http://www.w3.org/2000/svg"
-                          role="img"
-                          aria-label="Placeholder: 32x32"
-                          preserveAspectRatio="xMidYMid slice"
-                          focusable="false"
-                        >
-                          <title>Placeholder</title>
-                          <rect
-                            width="100%"
-                            height="100%"
-                            fill="#007bff"
-                          ></rect>
-                          <text x="50%" y="50%" fill="#007bff" dy=".3em">
-                            32x32
-                          </text>
-                        </svg> */}
-                        <img  width="48"
-                  height="38" className="bd-placeholder-img flex-shrink-0 me-2 rounded " src={p.img}/>
-
+              {state
+                ? state.map((p: any) => (
+                    <div key={p._id} className="d-flex text-body-secondary pt-3">
+                      <div className="col-1">
+                        <img
+                          width="48"
+                          height="38"
+                          className="bd-placeholder-img flex-shrink-0 me-2 rounded "
+                          src={p.img}
+                        />
+                      </div>
+                      <div className="col-10">
                         <p className="pb-3 mb-0 small lh-sm border-bottom">
-                          <strong className="d-block text-gray-dark">
-                            {" "}
-                            {p.name}
-                          </strong>
-                          Some representative placeholder content, with some
-                          information about this user. Imagine this being some
-                          sort of status update, perhaps?
+                        <strong className="d-block text-gray-dark">
+                          {p.name}
+                        </strong>
+                          {p.description}
                         </p>
                       </div>
-                    ))
-                  : ""}
+                      <div className="col-1">
+                        <i onClick={() => editModal(p._id, p.name, p.img, p.description)} className="fa-solid fa-pen-to-square cursor-pointer"></i>
+                        <i className="fa-solid fa-trash p-2 cursor-pointer"></i>
+                      </div>
+                    </div>
+                  ))
+                : ""}
 
-                <small className="d-block text-end mt-3">
-                  <a href="#">Crear nuevo</a>
-                </small>
-              </div>
-              <div className="col-6">
-              <p>FORMULARIO DE CREAR PROYECTO</p>
-              <div className="text-center">
-                <label>Nombre de proyecto</label>
-                <div className="form-control">
-                  <input
-                    className="form-control"
-                    type="text"
-                    onChange={(e) => handleProjectFormChange(e)}
-                    name="name"
-                    value={createProjectFrom.name}
-                  />
-                  <label>url de la imagen</label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    onChange={(e) => handleProjectFormChange(e)}
-                    name="img"
-                    value={createProjectFrom.img}
-                  />
-                  <button
-                    className="btn btn-primary mt-2"
-                    onClick={createProject}
-                  >
-                    create project
-                  </button>
+              <small className="d-block text-end mt-3">
+                <button type="button" className="btn" onClick={showModal}>
+                  Crear proyecto
+                </button>
+              </small>
+            </div>
+            {/* modal              */}
+            <div className="d-flex">
+            
+              <div
+                className="modal fade"
+                id="exampleModal"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="exampleModalLabel">
+                        Registro
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <label>Nombre de proyecto</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        onChange={(e) => handleProjectFormChange(e)}
+                        name="name"
+                        value={createProjectFrom.name}
+                      />
+                      <label>url de la imagen</label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        onChange={(e) => handleProjectFormChange(e)}
+                        name="img"
+                        value={createProjectFrom.img}
+                      />
+                      <label>Descripcion</label>
+                      <textarea name="description" 
+                       onChange={(e) => handleProjectFormChange(e)} 
+                       className="form-control"
+                       value={createProjectFrom.description}>
+                      </textarea>
+                      {/* <button
+                        className="btn btn-primary mt-2"
+                        onClick={createProject}
+                      >
+                        create project
+                      </button> */}
+                    </div>
+                    <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Cerrar
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          onClick={createProject}
+                        >
+                          Guardar
+                        </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            {/* <ul>
-          {
-            state ? 
-          state.map((p: any) => (
-              <li key={p.id}>{p.projectName}</li>
-          ))
-          : ""
-          }
-          
-        </ul> */}
+            
           </div>
         </div>
       </>
@@ -214,7 +273,7 @@ export default function Home() {
   return (
     <>
       <div className={styles.loginContainer}>
-      <div >
+        <div>
           <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
           <div className="form-floating">
             <input
@@ -239,33 +298,10 @@ export default function Home() {
             />
             <label>Password</label>
           </div>
-          <button
-            onClick={handleSubmit}
-            className="btn btn-primary w-100 py-2"
-          >
+          <button onClick={handleSubmit} className="btn btn-primary w-100 py-2">
             Sign in
           </button>
-          </div>
-      
-
-        {/* <div className={styles.loginFormContainer}>
-          <label>username</label>
-          <input
-            type="text"
-            name="username"
-            onChange={(e) => handleFormChange(e)}
-            value={loginForm.username}
-          />
-          <label> password</label>
-          <input
-            type="password"
-            name="password"
-            id=""
-            value={loginForm.password}
-            onChange={(e) => handleFormChange(e)}
-          />
-          <button className="btn btn-primary" onClick={handleSubmit}>Login</button>
-        </div> */}
+        </div>
       </div>
     </>
   );
